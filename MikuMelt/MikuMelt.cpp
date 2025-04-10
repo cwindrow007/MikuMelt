100927c70f4fdf18251fcd64d0418d2066b7a660
Chris <ChristoWindow@gmail.com>
2025-04-09 21:43:03 -0400

    Second Commit

--- MikuMelt/MikuMelt.cpp
+++ MikuMelt/MikuMelt.cpp
@@ -2,19 +2,36 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+//Entry Point for Ripper
+
+int main(int argc, char* argv[]) {
+	std::cout << "Welcome to Miku Melt Model Rippper (Consult Project README for more information)" << std::endl;
+
+	if (argc < 2) {
+		std::cout << "UsageL MikuMelt <path_to_file.gmo>" << std::endl;
+		return 1;
+	}
+
+	std::string filePath = argv[1];
+	std::ifstream file(filePath, std::ios::binary);
+
+	if (!file) {
+		std::cerr << "Failed to open file: " << filePath << std::endl;
+		return 1;
+	}
+
+	//Reads first 4 bytes (This is example code)
+	char header[4];
+	file.read(header, 4);
+
+	std::cout << "First 4 bytes (header): ";
+	for (int i = 0; i < 4; i++)
+		std::cout << std::hex << ((unsigned int)(unsigned char)header[i]) << " ";
+	std::cout << std::dec << std::endl;
+
+	file.close();
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
